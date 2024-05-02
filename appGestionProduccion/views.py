7d9525592bdc4819239adea3946a182af0b581a5
Jonay Román <jonay.roman@opendeusto.es>
2024-05-02 17:04:50 +0200

    terminar vista detallada procesos

--- appGestionProduccion/views.py
+++ appGestionProduccion/views.py
@@ -102,9 +102,10 @@ class ProcesoUpdateView(UpdateView):
         return render(request, 'appGestionProduccion/proceso_update.html', {'formulario': formulario})
 
 #Ver el detalle de un proceso
-def show_Proceso(request, proceso_id):
+def show_proceso(request, proceso_id):
     proceso = get_object_or_404(Proceso, id=proceso_id)
     equipo = proceso.equipos_asignados.all()
+
     return render(request, 'appGestionProduccion/proceso_detail.html', {'proceso':proceso, 'equipos':equipo})
 
 #Ver el detalle de un proceso
@@ -215,15 +216,8 @@ class OrdenDeleteView(DeleteView):
     model = Orden_De_fabricacion
     success_url = reverse_lazy('orden_list')
 
-#############################################################
 
-#4. ver los empleados asociados a un proceso
 
-def show_empleado(request, empleado_id):
-    empleado = get_object_or_404(Empleado, id=empleado_id)
-    output = (f'Detalles del empelado: {empleado.id}, {empleado.nombre},'
-              f' Empleados :{[e.nombre for e in empleado.proceso_set.all()]}')
-    return HttpResponse(output)
 
 
 
