a6637b38457678ecba282c0c3fea17aeafe301a0
Jon Toloas <j.tolosa@opendeusto.es>
2024-05-22 18:35:49 +0200

    Validar terminado, (falta enseñar por pantalla)

--- static/js/validar.js
+++ static/js/validar.js
@@ -1,61 +1,40 @@
 /*!--Validar campos del formulario--!*/
 
-function validarFormularioEquipo() {
-    var modelo = document.getElementById('modelo').value;
-    var marca = document.getElementById('marca').value;
-    var tipo = document.getElementById('tipo').value;
-    const fecha_adquisicion = new Date().toISOString().split('T')[0];
-    const fecha_instalacion = new Date().toISOString().split('T')[0];
-    var mensaje = document.getElementById('mensaje');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const marca = document.getElementById('id_marca');
+    const modelo = document.getElementById('id_modelo');
+    const tipo = document.getElementById('id_tipo');
 
-    // Realiza la validación comparando las fechas
-    if (fechaPuestaMarcha < fecha_adquisicion) {
-      mensaje.textContent = 'La fecha de puesta en marcha debe ser mayor o igual al día de hoy.';
-      return false;
-    } else {
-      mensaje.textContent = ''; // Limpiar el mensaje si los campos son válidos
-    }
+    marca.addEventListener('blur', (event) => {
+        const value = marca.value;
 
-    // Comprobamos que los campos tienen al menos 3 caracteres
-    if (marca.length < 3 || modelo.length < 3 || tipo.length < 3) {
-      mensaje.textContent = 'Los campos Marca, Modelo y Tipo de Equipo deben tener al menos 3 caracteres.';
-      return false;
-    } else {
-      mensaje.textContent = ''; // Limpiar el mensaje si los campos son válidos
-    }
+        if (value.length > 3) {
+            console.log('La longitud del texto es mayor a 3 caracteres.');
+        } else {
+            console.log('La longitud del texto es 3 o menos caracteres.');
+        }
+    });
 
-    // Si todas las validaciones son exitosas, enviar el formulario
-    return true;
-  }
+    modelo.addEventListener('blur', (event) => {
+        const value = modelo.value;
+    
+        if (value.length > 3) {
+            console.log('La longitud del texto es mayor a 3 caracteres.');
+        } else {
+            console.log('La longitud del texto es 3 o menos caracteres.');
+        }
+    });
 
-function validarFormularioEmpleado() {
+    tipo.addEventListener('blur', (event) => {
+        const value = tipo.value;
+    
+        if (value.length > 3) {
+            console.log('La longitud del texto es mayor a 3 caracteres.');
+        } else {
+            console.log('La longitud del texto es 3 o menos caracteres.');
+        }
+    });
+});
 
-    var dni = document.getElementById('dni').value;
-    var nombre = document.getElementById('nombre').value;
-    var apellidos = document.getElementById('apellidos').value;
-    var telefono = document.getElementById('telefono').value;
-    var email = document.getElementById('email').value;
-    var mensaje = document.getElementById('mensaje');
 
-    if (dni.length !== 9) {
-      mensaje.textContent = 'El DNI debe tener exactamente 9 caracteres.';
-      return false;
-    } else if (nombre.length < 3) {
-      mensaje.textContent = 'El nombre debe tener al menos 3 caracteres.';
-      return false;
-    } else if (apellidos.length < 3) {
-      mensaje.textContent = 'Los apellidos deben tener al menos 3 caracteres.';
-      return false;
-    } else if (telefono.length !== 9) {
-      mensaje.textContent = 'El teléfono debe tener exactamente 9 caracteres.';
-      return false;
-    } else if (!email.includes('@')) {
-      mensaje.textContent = 'El email debe tener una estructura válida.';
-      return false;
-    } else {
-      mensaje.textContent = ''; // Limpiar el mensaje si los campos son válidos
-      
-      // Si todas las validaciones son exitosas, enviar el formulario
-      return true;
-    }
-  }
+
