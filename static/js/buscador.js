cf1312c598db98378252271284a032c85a59f3e8
Jonay Román <jonay.roman@opendeusto.es>
2024-05-22 17:30:26 +0200

    mejora

--- static/js/buscador.js
+++ static/js/buscador.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const nombreBuscado = document.getElementById('input-buscador');
-    const sinResultadoDiv = document.querySelector('.sin-resultado');
-    const nadaCreadoDiv = document.querySelector('.nada-creado');
-    const searchQuerySpan = document.getElementById('nombre-buscado');
+const nombreBuscado = document.getElementById('input-buscador');
+const sinResultadoDiv = document.querySelector('.sin-resultado');
+const nadaCreadoDiv = document.querySelector('.nada-creado');
+const searchQuerySpan = document.getElementById('nombre-buscado');
 
-    if (nadaCreadoDiv) {
-        nadaCreadoDiv.style.display = 'none';
-        sinResultadoDiv.style.display = 'flex'
-        searchQuerySpan.textContent = nombreBuscado.value.trim();
-    }
-});
+if (nadaCreadoDiv) {
+    nadaCreadoDiv.style.display = 'none';
+    sinResultadoDiv.style.display = 'flex'
+    searchQuerySpan.textContent = nombreBuscado.value.trim();
+}
