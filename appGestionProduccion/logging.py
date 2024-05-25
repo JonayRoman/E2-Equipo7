52bbdd3dffae73c2dc8ab3e7bb081cd0dd24f332
Jonay Román <jonay.roman@opendeusto.es>
2024-05-25 11:14:29 +0200

    terminar y mejorar todas las funcionalidades Python y solucionar errores de la entrega anterior

--- appGestionProduccion/logging.py
+++ appGestionProduccion/logging.py
@@ -1,14 +1,14 @@
 import logging
 
-# Configuración del logger de creacion de proyectos
-logger = logging.getLogger('creacion_proceso')
+# Configuración del logger
+logger = logging.getLogger('LOG')
 logger.setLevel(logging.DEBUG)
 
 # Crear el archivo creacion-procesos.log
 file_handler = logging.FileHandler('Crear_Modificar.log')
 file_handler.setLevel(logging.DEBUG)
 
-# CFormatear los mensajes
+# Formatear los mensajes
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 
--- appGestionProduccion/views.py
+++ appGestionProduccion/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, DeleteView, UpdateView, DetailView
 import logging
-from .logging import logger as creacion_proceso_logger
+from .logging import logger as loggerCrearModificar
 
 from appGestionProduccion.forms import EmpleadoForm, ProcesoForm, EquipoForm, OrdenForm, EquipoUpdateForm, \
     ProcesoUpdateForm
@@ -18,19 +18,31 @@ class EmpleadoListView(ListView):
     template_name = "appGestionProduccion/empleado_list.html"
     context_object_name = "empleados"
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Empleado.objects.filter(nombre__icontains=query)
+        return Empleado.objects.all()
+
 #Crear un nuevo empleado
 class EmpleadoCreateView(View):
     def get(self, request):
         formulario = EmpleadoForm()
         context = {'formulario': formulario}
+        loggerCrearModificar.info('Has entrado a la pagina para crear un empleado')
         return render(request, 'appGestionProduccion/empleado_create.html', context)
 
     def post(self, request):
         formulario = EmpleadoForm(data=request.POST)
+        nombre = formulario.data.get('nombre', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('Nuevo empleado creado con el nombre "%s".', nombre)
             return redirect('empleado_list')
+        else:
+            loggerCrearModificar.warning('Error al crear el empleado: "%s".', nombre)
         return render(request, 'appGestionProduccion/empleado_create.html', {'formulario': formulario})
+
 
 #Eliminar un empleado
 class EmpleadoDeleteView(DeleteView):
@@ -47,16 +59,20 @@ class EmpleadoUpdateView(UpdateView):
             'formulario': formulario,
             'empleado': empleado
         }
+        loggerCrearModificar.info('Has entrado a la pagina para modificar "%s".', empleado.nombre)
         return render(request, 'appGestionProduccion/empleado_update.html', context)
     # Llamada para procesar la actualización del empleado
     def post(self, request, pk):
         empleado = Empleado.objects.get(id= pk)
         formulario = EmpleadoForm(request.POST, instance=empleado)
+        nombre = formulario.data.get('nombre', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('Modificacion exitosa del empleado "%s".', nombre)
             return redirect('empleado_list')
         else:
             formulario = EmpleadoForm(instance=empleado)
+            loggerCrearModificar.warning('Error al intentar realizar la modificacion para "%s".', nombre)
         return render(request, 'appGestionProduccion/empleado_update.html', {'formulario': formulario})
     
 #Lista de todos los procesos
@@ -76,17 +92,18 @@ class ProcesoCreateView(View):
     def get(self, request):
         formulario = ProcesoForm()
         context = {'formulario': formulario}
-        creacion_proceso_logger.info('Has entrado a la pagina para crear un proceso')
+        loggerCrearModificar.info('Has entrado a la pagina para crear un proceso')
         return render(request, 'appGestionProduccion/proceso_create.html', context)
 
     def post(self, request):
         formulario = ProcesoForm(data=request.POST, files=request.FILES)
+        proceso_nombre = formulario.data.get('nombre_proceso', 'desconocido')
         if formulario.is_valid():
             formulario.save()
-            creacion_proceso_logger.info('Nuevo proceso creado')
+            loggerCrearModificar.info('Nuevo proceso creado con el nombre "%s".', proceso_nombre)
             return redirect('proceso_list')
         else:
-            creacion_proceso_logger.warning('Error al crear el proceso: %s', formulario.errors)
+            loggerCrearModificar.warning('Error al crear el proceso: "%s".', proceso_nombre)
         return render(request, 'appGestionProduccion/proceso_create.html', {'formulario': formulario})
 
 # Eliminar un proceso
@@ -108,17 +125,20 @@ class ProcesoUpdateView(UpdateView):
             'formulario': formulario,
             'proceso': proceso
         }
-        creacion_proceso_logger.info('Has entrado a la pagina para modificar "%s".', proceso.nombre_proceso)
+        loggerCrearModificar.info('Has entrado a la pagina para modificar "%s".', proceso.nombre_proceso)
         return render(request, 'appGestionProduccion/proceso_update.html', context)
     # Llamada para procesar la actualización del proceso
     def post(self, request, pk):
         proceso = Proceso.objects.get(id= pk)
         formulario = ProcesoUpdateForm(request.POST, instance=proceso)
+        proceso_nombre = formulario.data.get('nombre_proceso', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('modificado con exito el proceso "%s".', proceso_nombre)
             return redirect('proceso_list')
         else:
             formulario = ProcesoForm(instance=proceso)
+            loggerCrearModificar.warning('Error al intentar realizar la modificacion del proceso "%s".', proceso_nombre)
         return render(request, 'appGestionProduccion/proceso_update.html', {'formulario': formulario})
 
 #Ver el detalle de un proceso
@@ -138,18 +158,29 @@ class EquipoListView(ListView):
     template_name = "appGestionProduccion/equipo_list.html"
     context_object_name = "equipos"
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Equipo.objects.filter(modelo__icontains=query)
+        return Equipo.objects.all()
+
 #Crear un nuevo equipo
 class EquipoCreateView(View):
     def get(self, request):
         formulario = EquipoForm()
         context = {'formulario': formulario}
+        loggerCrearModificar.info('Has entrado a la pagina para crear un equipo')
         return render(request, 'appGestionProduccion/equipo_create.html', context)
 
     def post(self, request):
         formulario = EquipoForm(data=request.POST, files=request.FILES)
+        equipo_modelo = formulario.data.get('modelo', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('Nuevo equipo creado cuyo modelo es "%s".', equipo_modelo)
             return redirect('equipo_list')
+        else:
+            loggerCrearModificar.warning('Error al crear el equipo con modelo: "%s".', equipo_modelo)
         return render(request, 'appGestionProduccion/equipo_create.html', {'formulario': formulario})
 
 #Eliminar un equipo
@@ -167,16 +198,20 @@ class EquipoUpdateView(UpdateView):
             'formulario': formulario,
             'empleado': equipo
         }
+        loggerCrearModificar.info('Has entrado a la pagina para modificar el equipo con modelo "%s".', equipo.modelo)
         return render(request, 'appGestionProduccion/equipo_update.html', context)
     # Llamada para procesar la actualización del equipo
     def post(self, request, pk):
         equipo = Equipo.objects.get(id= pk)
         formulario = EquipoUpdateForm(request.POST, instance=equipo)
+        equipo_modelo = formulario.data.get('modelo', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('modificado con exito el modelo "%s".', equipo_modelo)
             return redirect('equipo_list')
         else:
             formulario = EquipoForm(instance=equipo)
+            loggerCrearModificar.warning('Error al intentar realizar la modificacion del modelo "%s".', equipo_modelo)
         return render(request, 'appGestionProduccion/equipo_update.html', {'formulario': formulario})
 
 #vista detallada del equipo
@@ -195,18 +230,29 @@ class OrdenListView(ListView):
     template_name = "appGestionProduccion/orden_list.html"
     context_object_name = "ordenes"
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Orden_De_fabricacion.objects.filter(nombre__icontains=query)
+        return Orden_De_fabricacion.objects.all()
+
 #Crear nueva Orden de Fabricacion
 class OrdenCreateView(View):
     def get(self, request):
-        formulario = OrdenForm()  # Utiliza el formulario correspondiente
+        formulario = OrdenForm()
         context = {'formulario': formulario}
+        loggerCrearModificar.info('Has entrado a la pagina para crear una orden')
         return render(request, 'appGestionProduccion/orden_create.html', context)
 
     def post(self, request):
-        formulario = OrdenForm(data=request.POST)  # Utiliza el formulario correspondiente
+        formulario = OrdenForm(data=request.POST)
+        orden_nombre = formulario.data.get('nombre', 'desconocido')
         if formulario.is_valid():
             formulario.save()
-            return redirect('orden_list')  # Redirige a la lista de órdenes
+            loggerCrearModificar.info('Nueva orden creada con nombre "%s".', orden_nombre)
+            return redirect('orden_list')
+        else:
+            loggerCrearModificar.warning('Error al crear la orden "%s".', orden_nombre)
         return render(request, 'appGestionProduccion/orden_create.html', {'formulario': formulario})
     
 #Modificar una orden de fabricacion
@@ -215,20 +261,25 @@ class OrdenUpdateView(UpdateView):
     def get(self, request, pk):
         orden = Orden_De_fabricacion.objects.get(id=pk)
         formulario = OrdenForm( instance=orden)
+        orden_nombre = formulario.data.get('nombre', 'desconocido')
         context = {
             'formulario': formulario,
             'orden': orden
         }
+        loggerCrearModificar.info('Has entrado a la pagina para modificar la orden "%s".', orden_nombre)
         return render(request, 'appGestionProduccion/orden_update.html', context)
     # Llamada para procesar la actualización de la orden de fabricacion
     def post(self, request, pk):
         orden = Orden_De_fabricacion.objects.get(id=pk)
         formulario = OrdenForm(request.POST, instance=orden)
+        orden_nombre = formulario.data.get('nombre', 'desconocido')
         if formulario.is_valid():
             formulario.save()
+            loggerCrearModificar.info('modificada con exito la orden "%s".', orden_nombre)
             return redirect('orden_list')
         else:
             formulario = OrdenForm(instance=orden)
+            loggerCrearModificar.warning('Error al intentar realizar la modificacion de la orden "%s".', orden_nombre)
         return render(request, 'appGestionProduccion/orden_update.html', {'formulario': formulario})
     
 #Eliminar una orden de fabricacion
